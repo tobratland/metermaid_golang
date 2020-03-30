3d25425b434061f81c0df49ac7d429090b49c71f
Tobra <tobratland@gmail.com>
2020-03-30 12:32:31 +0200

    fixed testing method for storing and added testing method/mocks for getting all timeseries by time

--- repository/mocks/timeseriesRepository.go
+++ repository/mocks/timeseriesRepository.go
@@ -1,6 +1,8 @@
 package mocks
 
 import (
+	"time"
+
 	mock "github.com/stretchr/testify/mock"
 	"github.com/tobra/metermaid/models"
 )
@@ -10,7 +12,65 @@ type TimeseriesRepository struct {
 }
 
 func (_m *TimeseriesRepository) StoreValues(t *models.TimeSeries) error {
+	//TODO fix the test
 	ret := _m.Called(t)
+
+	var r1 error
+	if rf, ok := ret.Get(0).(func(*models.TimeSeries) error); ok {
+		r1 = rf(t)
+	} else {
+		r1 = ret.Error(0)
+	}
+
+	return r1
+}
+func (_m *TimeseriesRepository) StoreData(t *models.TimeSeries) error {
+	ret := _m.Called(t)
+
+	var r1 error
+	if rf, ok := ret.Get(0).(func(*models.TimeSeries) error); ok {
+		r1 = rf(t)
+	} else {
+		r1 = ret.Error(0)
+	}
+
+	return r1
+}
+
+func (_m *TimeseriesRepository) GetAllDataFromTimeToTime(from time.Time, to time.Time) ([]models.TimeSeries, error) {
+	ret := _m.Called(from, to)
+
+	var r0 []models.TimeSeries
+
+	if rf, ok := ret.Get(0).(func(time.Time, time.Time) []models.TimeSeries); ok {
+		r0 = rf(from, to)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).([]models.TimeSeries)
+		}
+	}
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func(time.Time, time.Time) error); ok {
+		r1 = rf(from, to)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+func (_m *TimeseriesRepository) GetValuesByTimeseries(t *models.TimeSeries) (*models.TimeSeries, error) {
+	ret := _m.Called(t)
+
+	var r0 *models.TimeSeries
+
+	if rf, ok := ret.Get(0).(func(*models.TimeSeries) *models.TimeSeries); ok {
+		r0 = rf(t)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(*models.TimeSeries)
+		}
+	}
 
 	var r1 error
 	if rf, ok := ret.Get(1).(func(*models.TimeSeries) error); ok {
@@ -18,6 +78,12 @@ func (_m *TimeseriesRepository) StoreValues(t *models.TimeSeries) error {
 	} else {
 		r1 = ret.Error(1)
 	}
+	return r0, r1
 
-	return r1
 }
+
+/*
+StoreData(t *models.TimeSeries) error
+	GetAllDataFromTimeToTime(from time.Time, to time.Time) ([]models.TimeSeries, error)
+	GetValuesByTimeseries(t *models.TimeSeries) (*models.TimeSeries, error)
+*/
--- usecase/usecase_test.go
+++ usecase/usecase_test.go
@@ -2,6 +2,7 @@ package usecase_test
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 
@@ -20,13 +21,45 @@ func TestStoreTimeseriesValues(t *testing.T) {
 	assert.NoError(t, err)
 
 	tempMockTimeseries := mockTimeseries
-	tempMockTimeseries.Id = "123test"
-	mockTimeseriesRepo.On("Store", &tempMockTimeseries).Return(mockTimeseries.Id, nil)
+	mockTimeseriesRepo.On("StoreValues", &tempMockTimeseries).Return(nil)
+	mockTimeseriesRepo.On("StoreData", &tempMockTimeseries).Return(nil)
 	defer mockTimeseriesRepo.AssertExpectations(t)
 
 	u := usecase.NewTimeseriesUsecase(mockTimeseriesRepo)
 
-	err = u.StoreValues(&tempMockTimeseries)
+	err = u.Store(&tempMockTimeseries)
 
 	assert.NoError(t, err)
+}
+
+func TestGetAllTimeseriesFromTimeToTime(t *testing.T) {
+	mockTimeseriesRepo := new(mocks.TimeseriesRepository)
+	var mockTimeseries models.TimeSeries
+	err := faker.FakeData(&mockTimeseries)
+	assert.NoError(t, err)
+	var mockTimeseriesList []models.TimeSeries
+	mockTimeseriesList = append(mockTimeseriesList, mockTimeseries)
+
+	from, err := time.Parse("2006-01-02T15:04:05Z", "1988-11-25T00:00:00Z")
+	assert.NoError(t, err)
+
+	to, err := time.Parse("2006-01-02T15:04:05Z", "1988-11-25T23:00:00Z")
+	assert.NoError(t, err)
+
+	mockTimeseriesRepo.On("GetAllDataFromTimeToTime", from, to).Return(mockTimeseriesList, err)
+	mockTimeseriesRepo.On("GetValuesByTimeseries", &mockTimeseries).Return(&mockTimeseries, err)
+
+	u := usecase.NewTimeseriesUsecase(mockTimeseriesRepo)
+
+	list, err := u.GetAllTimeseriesFromTimeToTime(from, to)
+	expected := mockTimeseriesList
+
+	assert.Equal(t, list, expected)
+	assert.NotEmpty(t, list)
+	assert.NoError(t, err)
+	assert.Len(t, list, len(mockTimeseriesList))
+
+	mockTimeseriesRepo.AssertCalled(t, "GetAllDataFromTimeToTime", from, to)
+	mockTimeseriesRepo.AssertCalled(t, "GetValuesByTimeseries", &mockTimeseries)
+
 }
